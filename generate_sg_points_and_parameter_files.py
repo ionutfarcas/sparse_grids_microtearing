20b371499a6ee5a51289b2921582e6f995a1532e
ionutfarcas <ionut.gabriel.farcas@gmail.com>
2024-06-25 19:39:25 -0400

    updated point generation to compute amhd self-consistenly

--- generate_sg_points_and_parameter_files.py
+++ generate_sg_points_and_parameter_files.py
@@ -6,6 +6,19 @@ import numpy as np
 import os
 import shutil
 
+from scipy.optimize import root
+
+## other parameters
+Lref    = 1.6832814164953476
+Bref    = 2.0167777970779888
+major_R = 1.0
+
+densi       = 0.8
+tempe       = 1.0000000 
+dense       = 1.0000000 
+densz       = 0.03333
+##
+
 # shat
 left_shat  	= 3.8
 right_shat  = 21.7
@@ -45,7 +58,7 @@ right_omt_e   = 360.0
 
 if __name__ == '__main__':
 
-    dim = 9 # number of parameters in the scan
+    dim = 8 # number of parameters in the scan
     
     ############### only these three parameters should be modified ############
     level 	 = 4 # grid level; this tells us the maximum monomial degree in each direction (which is level - 1)
@@ -74,13 +87,52 @@ if __name__ == '__main__':
    
     sg_shat     = map_rv(left_shat, right_shat, sg_points[:, 0])
     sg_coll     = map_rv(left_coll, right_coll, sg_points[:, 1])
-    sg_amhd     = map_rv(left_amhd, right_amhd, sg_points[:, 2])
-    sg_q0       = map_rv(left_q0, right_q0, sg_points[:, 3])
-    sg_beta     = map_rv(left_beta, right_beta, sg_points[:, 4])
-    sg_temp_i   = map_rv(left_temp_i, right_temp_i, sg_points[:, 5])
-    sg_omn      = map_rv(left_omn, right_omn, sg_points[:, 6])
-    sg_omt_i    = map_rv(left_omt_i, right_omt_i, sg_points[:, 7])
-    sg_omt_e    = map_rv(left_omt_e, right_omt_e, sg_points[:, 8])
+    # sg_amhd     = map_rv(left_amhd, right_amhd, sg_points[:, 2])
+    sg_q0       = map_rv(left_q0, right_q0, sg_points[:, 2])
+    sg_beta     = map_rv(left_beta, right_beta, sg_points[:, 3])
+    sg_temp_i   = map_rv(left_temp_i, right_temp_i, sg_points[:, 4])
+    sg_omn      = map_rv(left_omn, right_omn, sg_points[:, 5])
+    sg_omt_i    = map_rv(left_omt_i, right_omt_i, sg_points[:, 6])
+    sg_omt_e    = map_rv(left_omt_e, right_omt_e, sg_points[:, 7])
+
+    amhd = np.zeros_like(sg_shat)
+
+    for n in range(n_sg):
+
+        print('calculating amhd for sg point {}'.format(n + 1))
+
+        ########### amhd calculation ###################
+        c0 = sg_beta[n]/(403e-5)*Bref**2
+
+        def coll_func(nref):
+            return -sg_coll[n] + 2.3031E-5*Lref*(nref)/(c0/nref)**2*(24.-np.log(np.sqrt(nref*1.0E13)/(c0/nref)*0.001))
+
+        # Initial guess for nref
+        initial_guess = 1.0
+
+        # Use scipy.optimize.root to find the root of coll_func
+        result = root(coll_func, initial_guess)
+
+        if result.success:
+            nref_root = result.x[0]
+
+            print("nref from root finder:",nref_root)
+        else:
+            print("The root finding did not converge.")
+
+            nref = nref_root
+            Tref = c0/nref
+
+            print("nref=", nref)
+            print("Tref=", Tref)
+
+        amhd[n] = sg_q0[n]**2*major_R*sg_beta[n]*((sg_omn[n] + sg_omt_i[n])*sg_temp_i[n]*densi + (sg_omn[n] + sg_omt_e[n])*tempe*dense + \
+                (sg_omn[n] + sg_omt_i[n])*sg_temp_i[n]*densz)
+
+        print("amhd = ", amhd[n])
+
+    print('****************************')
+    ################################################
 
     for f in range(n_sg):
         if not os.path.isdir(dir_name(f)):
@@ -96,7 +148,7 @@ if __name__ == '__main__':
 					out_dir(n) + ' ' + \
                     str(sg_shat[n]) + ' ' + \
 					str(sg_coll[n]) + ' ' + \
-					str(sg_amhd[n]) + ' ' + \
+					str(amhd[n]) + ' ' + \
 					str(sg_q0[n]) + ' ' + \
 					str(sg_beta[n]) + ' ' + \
 					str(sg_temp_i[n]) + ' ' + \
